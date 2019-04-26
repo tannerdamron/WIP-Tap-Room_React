f1a79247f8ea9cc8fe0762d06cb2d5a6a17e81d3
Tanner Damron <tanner.mdamron@yahoo.com>
2019-04-26 08:28:29 -0700

    add file structure

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <style jsx global>{`
+        
+        `}</style>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
